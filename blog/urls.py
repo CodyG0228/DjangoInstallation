db197f56950f6b5ee6fb33f1754a5d04acb591c4
Cody G. <cgunter13@ivytech.edu>
2026-04-15 15:30:29 -0400

    Added view, front end for posts.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views 
+
+
+#CREATE A LIST OF URL PATTERNS THAT WHEN A URL IS VISITED, IS TIED TO A FUNCTION CALL
+
+urlpatterns = [
+    path('',views.post_list,name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    #RETURN TO THE REQUESTER THE HTML PAGE AND THE DATA THEY ARE ASKING FOR
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
