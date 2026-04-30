5025cc61e1cca8d73bdfbb5e62be633273c8a014
Cody G. <cgunter13@ivytech.edu>
2026-04-30 12:39:55 -0400

    Completed everything up to and including "Security" in the DjangoGirls tutorial. Had an issue with conflicting changes between my PC and laptop, that was resolved.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    #RETURN TO THE REQUESTER THE HTML PAGE AND THE DATA THEY ARE ASKING FOR
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
@@ -25,4 +24,18 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
